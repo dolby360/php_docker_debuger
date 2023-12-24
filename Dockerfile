23f10d2400682b8cab6c0261b6cb2ecca267da3a
dolby360 <dolevben054@gmail.com>
2023-12-24 20:56:43 +0200

    natas14

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,27 @@
 FROM php:8.2-cli
 
-RUN apt-get update 
-RUN apt-get install -y lsof procps
-RUN pecl install -o -f xdebug
-RUN docker-php-ext-enable xdebug
+# Install required packages
+RUN apt-get update \
+    && apt-get install -y lsof procps \
+    && docker-php-ext-install pdo_mysql
 
+# Install Xdebug
+RUN pecl install -o -f xdebug \
+    && docker-php-ext-enable xdebug
+
+# Copy PHP configuration
 COPY ./php.ini /usr/local/etc/php/
 
+# Copy the application code
 COPY . /var/www
 WORKDIR /var/www
 
-RUN mkdir -p /etc/natas_webpass
-RUN echo "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" > /etc/natas_webpass/natas10
+# Create MySQL database and table
+RUN mkdir -p /etc/natas_webpass \
+    && service mysql start \
+    && mysql -e "CREATE DATABASE IF NOT EXISTS ctf;" \
+    && mysql -e "USE ctf; CREATE TABLE IF NOT EXISTS users (username VARCHAR(255), password VARCHAR(255));" \
+    && mysql -e "USE ctf; INSERT INTO users (username, password) VALUES ('argus', 'https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1000&h=563&crop=1&resize=1000%2C563');"
 
+# Set up CMD for running PHP server
 CMD ["php", "-S", "0.0.0.0:8000", "-t", "."]
--- index.php
+++ index.php
@@ -1,31 +1,47 @@
 <html>
 <head>
+<!-- This stuff in the header has nothing to do with the level -->
 <link rel="stylesheet" type="text/css" href="http://natas.labs.overthewire.org/css/level.css">
 <link rel="stylesheet" href="http://natas.labs.overthewire.org/css/jquery-ui.css" />
 <link rel="stylesheet" href="http://natas.labs.overthewire.org/css/wechall.css" />
+
 </head>
 <body>
-<h1>Argus CTF Day</h1>
+<h1>Argus CTF day</h1>
 <div id="content">
+<?php
 
-<form>
-Find words containing: <input name=needle><input type=submit name=submit value=Search><br><br>
-</form>
-<?
-
-$key = "";
-
-if(array_key_exists("needle", $_REQUEST)) {
-    $key = $_REQUEST["needle"];
-}
-
-if($key != "") {
-    passthru("grep -i $key dictionary.txt");
-}
+$host = 'localhost';
+$dbname = 'ctf';
+$username = 'root';
+$password = ''; 
 
 
+if(array_key_exists("username", $_REQUEST)) {
+    $link = mysqli_connect($host, $username, $password);
+    mysqli_select_db($link, $dbname);
+
+    $query = "SELECT * from users where username=\"".$_REQUEST["username"]."\" and password=\"".$_REQUEST["password"]."\"";
+    if(array_key_exists("debug", $_GET)) {
+        echo "Executing query: $query<br>";
+    }
+
+    if(mysqli_num_rows(mysqli_query($link, $query)) > 0) {
+            echo "Successful login! The password for natas15 is <censored><br>";
+    } else {
+            echo "Access denied!<br>";
+    }
+    mysqli_close($link);
+} else {
 ?>
 
+<form action="index.php" method="POST">
+Username: <input name="username"><br>
+Password: <input name="password"><br>
+<input type="submit" value="Login" />
+</form>
+<?php } ?>
+<div id="viewsource"><a href="index-source.html">View sourcecode</a></div>
 </div>
 </body>
 </html>
